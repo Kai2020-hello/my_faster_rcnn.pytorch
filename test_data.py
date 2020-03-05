c5aa9d53ca7ba04607eea7e7eff48ad3984f19ff
zhangzhongkai <zhangzhongkai@meicai.cn>
2020-03-05 10:25:28 +0800

    测试数据生成

--- test_data.py
+++ test_data.py
@@ -24,8 +24,8 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import Sampler
 
-from roi_data_layer.roidb import combined_roidb
-from roi_data_layer.roibatchLoader import roibatchLoader
+from lib.roi_data_layer.roidb import combined_roidb
+from lib.roi_data_layer.roibatchLoader import roibatchLoader
 
 
 
@@ -33,41 +33,27 @@ from roi_data_layer.roibatchLoader import roibatchLoader
 if __name__ == '__main__':
 
 
-    args = parse_args()
+    batch_size = 12
 
-    print('Called with args:')
-    print(args)
+    imdb_name = "coco_2014_train+coco_2014_valminusminival"
+    imdbval_name = "coco_2014_minival"
+    set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '50']
 
-    args.imdb_name = "coco_2014_train+coco_2014_valminusminival"
-    args.imdbval_name = "coco_2014_minival"
-    args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '50']
-
-
-    np.random.seed(cfg.RNG_SEED)
-
-    #torch.backends.cudnn.benchmark = True
-    if torch.cuda.is_available() and not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-
-    # train set
-    # -- Note: Use validation set and disable the flipped to enable faster loading.
-    cfg.TRAIN.USE_FLIPPED = True
-    cfg.USE_GPU_NMS = args.cuda
-    imdb, roidb, ratio_list, ratio_index = combined_roidb(args.imdb_name)
+    imdb, roidb, ratio_list, ratio_index = combined_roidb(imdb_name)
     train_size = len(roidb)
 
     print('{:d} roidb entries'.format(len(roidb)))
 
-    output_dir = args.save_dir + "/" + args.net + "/" + args.dataset
-    if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
-
-    sampler_batch = sampler(train_size, args.batch_size)
-
-    dataset = roibatchLoader(roidb, ratio_list, ratio_index, args.batch_size, \
-                             imdb.num_classes, training=True)
-
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size,
-                              sampler=sampler_batch, num_workers=args.num_workers)
-
     
+    sampler_batch = sampler(train_size, batch_size)
+    
+    dataset = roibatchLoader(roidb, ratio_list, ratio_index, batch_size, \
+                             imdb.num_classes, training=True)
+    
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                              sampler=sampler_batch, num_workers=1)
+    
+    
+    data = next(data_iter)
+    print(data)
+
